dcacf370d3dce9f82971c37fcd0c80d0fd41e7d7
cezzzanne <pablo.arellanos97@gmail.com>
2025-12-02 16:24:05 -0800

    package lock

--- api/og.js
+++ api/og.js
@@ -4,32 +4,31 @@ export const config = { runtime: 'edge' };
 
 export default function handler(request) {
   const { searchParams } = new URL(request.url);
-  
+
   const hexColor = searchParams.get('hexColor') || '#FF7F50';
-  const title = searchParams.get('title') || 'Playing beer pong at a house party';
+  const textColor = '#6D1F08';
+  const title = searchParams.get('title') || 'Playing beer pong at a house party in Sugar House';
 
   return new ImageResponse(
-    {
-      type: 'div',
-      props: {
-        style: {
+    (
+      <div
+        style={{
           height: '100%',
           width: '100%',
           display: 'flex',
+          flexDirection: 'column',
           backgroundColor: hexColor,
           padding: '60px',
+          fontFamily: 'sans-serif',
           justifyContent: 'center',
           alignItems: 'center',
-        },
-        children: {
-          type: 'div',
-          props: {
-            style: { fontSize: 48, fontWeight: 900, color: '#6D1F08' },
-            children: title,
-          },
-        },
-      },
-    },
+        }}
+      >
+        <div style={{ fontSize: 48, fontWeight: 900, color: textColor }}>
+          {title}
+        </div>
+      </div>
+    ),
     {
       width: 1200,
       height: 630,
