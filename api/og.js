79c9d31fc77851a9734a1bd8212f07466a0ebefb
cezzzanne <pablo.arellanos97@gmail.com>
2025-12-02 16:23:11 -0800

    package lock

--- api/og.js
+++ api/og.js
@@ -2,35 +2,37 @@ import { ImageResponse } from '@vercel/og';
 
 export const config = { runtime: 'edge' };
 
-export default function handler(request: Request) {
+export default function handler(request) {
   const { searchParams } = new URL(request.url);
   
   const hexColor = searchParams.get('hexColor') || '#FF7F50';
   const title = searchParams.get('title') || 'Playing beer pong at a house party';
 
   return new ImageResponse(
-    (
-      <div
-        style={{
+    {
+      type: 'div',
+      props: {
+        style: {
           height: '100%',
           width: '100%',
           display: 'flex',
-          flexDirection: 'column',
           backgroundColor: hexColor,
           padding: '60px',
-          fontFamily: 'sans-serif',
           justifyContent: 'center',
           alignItems: 'center',
-        }}
-      >
-        <div style={{ fontSize: 48, fontWeight: 900, color: '#6D1F08' }}>
-          {title}
-        </div>
-      </div>
-    ),
+        },
+        children: {
+          type: 'div',
+          props: {
+            style: { fontSize: 48, fontWeight: 900, color: '#6D1F08' },
+            children: title,
+          },
+        },
+      },
+    },
     {
       width: 1200,
       height: 630,
-    },
+    }
   );
 }
